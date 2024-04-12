1d311fe2bb0450b747e0cc390a8825a2b47e7988
Primeister <160356474+Primeister@users.noreply.github.com>
2024-04-12 13:08:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
-app.get('/message', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
