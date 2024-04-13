f6f0ebd2859caaea63aad34d1da3ec9b867bf02e
Primeister <160356474+Primeister@users.noreply.github.com>
2024-04-13 23:54:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 let cars = ["cars"];
 
 const port = process.env.PORT || 3000;
