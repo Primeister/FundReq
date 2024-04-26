22304d7ce05b2c77d259add85dd1877ca093effa
Siphosakhe2583003 <160494935+Siphosakhe2583003@users.noreply.github.com>
2024-04-26 18:14:53 +0200

    updating server for signin and login

--- server.js
+++ server.js
@@ -75,51 +75,66 @@ app.post('/fundManagers/advert/post/:id', (req, res) => {
 
 // Route to handle user registration
 app.post("/register",(req, res) => {
-    let { username, name, surname, email, password, userType } = req.body;
+    let { name, surname, email, userType} = req.body;
     
-    // Hash the password for security reasons
-    bcrypt.genSalt(10, (err, salt) => {
-        if (err) {
-            console.error("Error generating salt:", err);
-            res.status(500).json({ error: "Error registering user" });
-        } else {
-            bcrypt.hash(password, salt, (err, hash) => {
-                if(err){
-                    console.error("Error hashing password:", err);
-                    res.status(500).json({ error: "Error registering user" });
-                }
-                else {
-                    password = hash;
-                    const sql = `INSERT INTO ${userType} (name, surname, email, username, password) VALUES (?, ?, ?, ?, ?)`;
-                    const values = [name, surname, email, username, password];
+    const sql = `INSERT INTO ${userType} (name, surname, email) VALUES (?, ?, ?)`;
+    const values = [name, surname, email];
                     
-                    // Checking if email has already been used
-                    const sqlCheck = `SELECT * FROM ${userType} WHERE email = ?`;
+    // Checking if email has already been used
+    const sqlCheck = `SELECT * FROM ${userType} WHERE email = ?`;
 
-                    db.get(sqlCheck, [email], (err, row) => {
-                        if (err) {
-                            console.error("Error checking email:", err);
-                            res.status(500).json({ error: "Error registering user" });
-                        } else if (!row) {
-                            // No user found with the provided email
-                            db.run(sql, values, function(err) {
-                                if (err) {
-                                    console.error("Error registering user:", err);
-                                    res.status(500).json({ error: "Error registering user" });
-                                } else {
-                                    res.status(201).json({ message: "User registered successfully", userId: this.lastID });
-                                }
-                            });
-                        } else {
-                            console.error("Email already used, try signing in.");
-                            res.status(500).json({ error: "Email already used, try signing in." });
-                        }
-                    });
+    db.get(sqlCheck, [email], (err, row) => {
+        if (err) {
+            console.error("Error checking email:", err);
+            res.status(500).json({ error: "Error registering user" });
+        } else if (!row) {
+            // No user found with the provided email
+            db.run(sql, values, function(err) {
+                if (err) {
+                    console.error("Error registering user:", err);
+                    res.status(500).json({ error: "Error registering user" });
+                } else {
+                    res.status(201).json({ message: "User registered successfully", userId: this.lastID, user: { name, surname, email, userType}});
                 }
             });
+        } else {
+            const tables = ['funders', 'applicants', 'admins'];
+        
+
+            // Function to perform login check for a specific table
+            const checkLogin = (table) => {
+                const sql = `SELECT * FROM ${table} WHERE email = ?`;
+                const values = [email];
+            
+                db.get(sql, values, (err, row) => {
+                    if (err) {
+                        console.error("Error logging in:", err);
+                        res.status(500).json({ error: "Error logging in" });
+                    } else if (!row) {
+                        // User not found in this table
+                        const index = tables.indexOf(table);
+                        if (index === tables.length - 1) {
+                            // User not found in any table
+                            res.status(401).json({ error: "Invalid username or password" });
+                        } else {
+                            // Check next table
+                            checkLogin(tables[index + 1]);
+                        }
+                    } else {
+                        // User found, send success response
+                        res.status(200).json({ message: "User registered successfully", user: {...row, userType: table}});
+                    }
+                });
+            };
+            
+            
+            // Start login check with the first table
+            checkLogin(tables[0]);
+                
         }
     });
 });
+
 
 
 
@@ -130,8 +145,7 @@ app.get("/login", (req, res) => {
 
 // Route to handle user login
 app.post("/login", (req, res) => {
-    let username;
-    let { email, password } = req.body;
+    let { email } = req.body;
 
     // Define tables to check
     const tables = ['funders', 'applicants', 'admins'];
@@ -140,58 +154,48 @@ app.post("/login", (req, res) => {
     const checkLogin = (table) => {
         const sql = `SELECT * FROM ${table} WHERE email = ?`;
         const values = [email];
-
+    
         db.get(sql, values, (err, row) => {
             if (err) {
                 console.error("Error logging in:", err);
                 res.status(500).json({ error: "Error logging in" });
             } else if (!row) {
                 // User not found in this table
-                if (table === tables[tables.length - 1]) {
+                const index = tables.indexOf(table);
+                if (index === tables.length - 1) {
                     // User not found in any table
                     res.status(401).json({ error: "Invalid username or password" });
                 } else {
                     // Check next table
-                    checkLogin(tables[tables.indexOf(table) + 1]);
+                    checkLogin(tables[index + 1]);
                 }
             } else {
-                // User found in this table, compare passwords
-                bcrypt.compare(password, row.password, (err, result) => {
-                    if (err) {
-                        console.error("Error comparing passwords:", err);
-                        res.status(500).json({ error: "Error logging in" });
-                    } else if (result) {
-                        // Passwords match, user authenticated successfully
-                        res.status(200).json({ message: "Login successful", userId: row.id, userType: table, username: row.username });
-                    } else {
-                        // Passwords don't match
-                        res.status(401).json({ error: "Invalid username or password" });
-                    }
-                });
+                // User found, send success response
+                res.status(200).json({ message: "User logged in successfully", user: row });
             }
         });
-    };
-
+    }
+    
+    
     // Start login check with the first table
     checkLogin(tables[0]);
 });
 
 // Define a route to handle the update operation
 app.put('/update/status/:id', (req, res) => {
-    const id = req.params.id;
-    const newValue = req.body.newValue; // Assuming the new value is passed in the request body 
+     const id = req.params.id;
+      const newValue = req.body.newValue; // Assuming the new value is passed in the request body 
 
     // Run the update query
-    const sql = `UPDATE funders SET status = ? WHERE id = ?`;
-    db.run(sql, [newValue, id], function(err) {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.json({
-            message: "Field updated successfully",
-            changes: this.changes // Number of rows affected
-        });
-    });
+ const sql = `UPDATE funders SET status = ? WHERE id = ?`;
+    db.run(sql, [newValue, id], function(err) {
+     if (err) {
+        return res.status(500).json({ error: err.message });
+     }
+     res.json({
+         message: "Field updated successfully", changes: this.changes // Number of rows affected
+     });
+     });
 });
 
 
@@ -211,3 +215,4 @@ app.get("/profiles", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port${PORT}`);
 });
+
